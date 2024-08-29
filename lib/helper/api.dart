7b98df284a76cc5983c7024f9d8b7cc1dc70a53e
Mohammed Hashem Bdier <mhb20033@gmail.com>
2024-08-29 17:19:07 +0300

    update product service

--- lib/helper/api.dart
+++ lib/helper/api.dart
@@ -3,10 +3,23 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class Api {
-  Future<dynamic> get({required String url}) async {
+  Future<dynamic> get({
+    required String url,
+    String? token,
+  }) async {
+    Map<String, String> headers = {};
+    if (token != null) {
+      headers.addAll(
+        {
+          "Authorization": "Bearer $token",
+        },
+      );
+    }
     http.Response response = await http.get(
       Uri.parse(url),
+      headers: headers,
     );
+
     if (response.statusCode == 200) {
       return jsonDecode(response.body);
     } else {
