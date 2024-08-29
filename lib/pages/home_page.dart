c7f6f9eb4098c8ba521bf2b0001a0ed97c67b8b1
Mohammed Hashem Bdier <mhb20033@gmail.com>
2024-08-29 18:51:50 +0300

    product card

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -19,7 +19,63 @@ class HomePage extends StatelessWidget {
           ),
         ],
       ),
-      body: Container(),
+      body: Center(
+        child: Container(
+          width: 220,
+          height: 130,
+          decoration: BoxDecoration(
+            boxShadow: [
+              BoxShadow(
+                  blurRadius: 40,
+                  color: Colors.grey.withOpacity(0.2),
+                  spreadRadius: 0,
+                  offset: const Offset(10, 10)),
+            ],
+          ),
+          child: Card(
+            elevation: 10,
+            color: Colors.white,
+            child: Padding(
+              padding: const EdgeInsets.all(16),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    "HndBag LV",
+                    style: TextStyle(
+                      color: Colors.grey,
+                      fontSize: 16,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      const Text(
+                        r"225$",
+                        style: TextStyle(
+                          fontSize: 16,
+                        ),
+                      ),
+                      IconButton(
+                        onPressed: () {},
+                        icon: const Icon(
+                          FontAwesomeIcons.solidHeart,
+                          color: Colors.red,
+                          size: 16,
+                        ),
+                      )
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
